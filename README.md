e6504fa70997a01ac818df0dab29edf14878997f
c0f <srv@o9.org>
2023-01-26 17:25:18 +0000

    README update and comment tidy

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
-# SimpleRandomPhotoSlideshow
+# Simple Random Photo Slideshow
 
-This is a simple slideshow programme that scans a directory for photos and displays them in a random order. The scanned directory can contain images or subdirectories containing images.
+This is a simple slideshow programme that scans a directory for images and displays them in a random order.
+
+The scanned directory can contain images or subdirectories containing images.
 
 The slideshow is configured using variables at the top of the slideshow.php file:
 
 * $interval - this sets the number of seconds a photo is displayed before the next photo is displayed.
 * $photoDir - this is the path where photos or directories of photos are stored. This must be relative to the slideshow.php file.
-* $backgroundColor & $textColor - the background and text colors.
+* $photoExt - the file extension that will be search for. Only one extension is supported but it is not case sensitive.
 * $rescanAfter - this is the number of minutes after which $photoDir will be rescanned.
+* $backgroundColor & $textColor - the background and text colors.
 
 
 ### Acknowledgements
-I am not an experienced PHP developer and this programme would not work without a block of code from Stack Overflow that is used to scan directories. The getDirContents() function was written user2226755
+I am not an experienced PHP developer and this programme would not work without a block of code from Stack Overflow that is used to scan directories. The getDirContents() function was written user2226755 and is available at: 
 
-URL: https://stackoverflow.com/questions/24783862/list-all-the-files-and-folders-in-a-directory-with-php-recursive-function
+https://stackoverflow.com/questions/24783862/list-all-the-files-and-folders-in-a-directory-with-php-recursive-function
 
 
--- slideshow.php
+++ slideshow.php
@@ -3,26 +3,20 @@
 // Interval is the number of seconds between each photo
 $interval = '10';
 
-// photoDir is the directory containing photos and/or subdirectories of photos
-// The path is relative to the directory containing this php file
+// photoDir is the directory containing photos and/or subdirectories of photos. The path is relative to the directory containing this php file
 $photoDir = '../photos';
-//$photoDir = './photos';
 
-// photoExt is the file extension of the photos.
-// Do not include '.'.
-// Not case-sensitive.
+// photoExt is the file extension of the photos. Do not include '.'. Not case-sensitive.
 $photoExt = 'jpg';
 
 // Background and text colors
 $backgroundColor = 'black';
 $textColor = 'cyan';
 
-// Number of minutes after which the photo directory will be rescanned
-// The rescan is triggered by recreating the session to force a file rescan
+// Number of minutes after which the photo directory will be rescanned. The rescan is triggered by recreating the session to force a file rescan
 $recanAfter = 720;
 
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -44,10 +38,8 @@ $recanAfter = 720;
 </head>
 <body>
 <?php
-
 // Session variables are used to prevent rescaning photo folders every time the page is refreshed.
 session_start();
-
 
 // Function getDirContents was written by stackoverflow user user2226755
 // URL: https://stackoverflow.com/questions/24783862/list-all-the-files-and-folders-in-a-directory-with-php-recursive-function
@@ -83,9 +75,6 @@ if($arrayAge > ($recanAfter * 60)){
 // Get a random array index
 $random = array_rand($_SESSION['photos'],1);
 
-// Get the full path and filename of a random photo and strip text from the path and filename
-//$photo = str_replace($_SERVER['DOCUMENT_ROOT'],"..",$_SESSION['photos'][$random]);
-
 // Get the path and filename of the random photo
 // Remove the filesystem directory name from scandir() results
 $photo = str_replace($_SERVER['DOCUMENT_ROOT'],"",$_SESSION['photos'][$random]);
@@ -98,8 +87,6 @@ $photoDateTimeOriginal = $photoexif['DateTimeOriginal'];
 echo("<pre>" . basename($photo) . " (Original Date $photoDateTimeOriginal)" . "</pre>");
 
 ?>
-
 <img src="<?=$photo?>"/>
-
 </body>
 </html>
