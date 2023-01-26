e70f536247b1c27be09d7d23a31911cea2f3dae5
c0f <srv@o9.org>
2023-01-26 21:55:36 +0000

    Added <meta name="apple-mobile-web-app-capable" content="yes"> for full-screen use on iOS when run from an iOS Home Screen link.

--- slideshow.php
+++ slideshow.php
@@ -23,6 +23,7 @@ $recanAfter = 720;
  <title>Simple Random Photo Slideshow</title>
  <meta http-equiv="refresh" content="<?=$interval?>" >
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="apple-mobile-web-app-capable" content="yes">
  <style>
  img {
    object-fit: contain;
